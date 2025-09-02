67cad4a9c11df026720ef797ff70f7fe0ec1774c
jeronimoguevararogel <guevararogeljj@icloud.com>
2025-09-02 13:55:57 -0600

    update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,24 +1,11 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://appointmentsgateway-egfpavfabyehg0dp.mexicocentral-01.azurewebsites.net/api/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://localhost:7001/api/v1',
   headers: {
     'Content-Type': 'application/json',
-    'XApiKey': 'pgH7QzFHJx4w46fI~5Uzi4RvtTwlEXp',
+    'X-API-KEY': 'pgH7QzFHJx4w46fI~5Uzi4RvtTwlEXp',
   },
 });
-
-apiClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('jwtToken');
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
 
 export default apiClient;
